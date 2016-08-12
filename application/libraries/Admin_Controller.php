fe7cbfd7643977555981362c59addfb825bd02ae
Alexander Velichko <alvi@mail.ua>
2016-08-12 12:18:28 +0300

    create login form

--- application/libraries/Admin_Controller.php
+++ application/libraries/Admin_Controller.php
@@ -6,6 +6,9 @@ class Admin_Controller extends MY_Controller
     {
         parent::__construct();
         $this->data['meta_title']  = 'My Cms';
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+        $this->load->model('user_m');
 
 
     }
--- application/views/admin/_layout_modal.php
+++ application/views/admin/_layout_modal.php
@@ -1,21 +1,14 @@
 <? $this->load->view('admin/components/page_head'); ?>
     <body>
-    div class="modal fade" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                        aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">page title</h4>
-            </div>
-            <div class="modal-body">
-                <p>One fine body&hellip;</p>
-            </div>
-            <div class="modal-footer">
-                <?= $meta_title; ?>
-            </div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
+    <div class="" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <? $this->load->view( $subview ); ?>
+                <div class="modal-footer">
+                    &copy;<?= date('Y'); ?>  <?= $meta_title; ?>
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
     </body>
 <? $this->load->view('admin/components/page_footer'); ?>
